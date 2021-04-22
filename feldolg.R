733c0304bf7b79d982beba013ad02c620f2c987d
Péter Kalicz <kaliczp@gmail.com>
2021-04-22 10:48:21 +0200

    Adatok kinyerése

--- feldolg.R
+++ feldolg.R
@@ -2,4 +2,7 @@
 library(readxl)
 s17 <- read_excel("Soil moisture 2017_2020.xlsx", "2017")
 ## Dátum létrehozása
-s.dat <- as.Date(as.data.frame(s17[,1])[,1])
+s.dat <- as.Date(as.data.frame(s17)[,1])
+## Adatok beolvasása
+s.adat <- as.data.frame(s17)[,2:6]
+names(s.adat) <- paste0("Forest",1:5)
