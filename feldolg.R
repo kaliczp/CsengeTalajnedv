83b92c6b8f1d6715a58cb4438dfc3c9044e7836c
Péter Kalicz <kaliczp@gmail.com>
2021-04-22 10:58:12 +0200

    Beolvasás az összes mintavételi pontra

--- feldolg.R
+++ feldolg.R
@@ -4,5 +4,6 @@ s17 <- read_excel("Soil moisture 2017_2020.xlsx", "2017")
 ## Dátum létrehozása
 s.dat <- as.Date(as.data.frame(s17)[,1])
 ## Adatok beolvasása
-s.adat <- as.data.frame(s17)[,2:6]
-names(s.adat) <- paste0("Forest",1:5)
+s.adat <- as.data.frame(s17)[,2:16]
+names(s.adat) <- paste0(rep(c("Forest","Forest_edge","Meadow"), each = 5), 1:5)
+s.adat[,10:15] <- numeric(nrow(s.adat))
