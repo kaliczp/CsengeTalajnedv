7979582baf44707bd20d6aba5d09fa9e4c94e79a
Péter Kalicz <kaliczp@gmail.com>
2021-04-22 11:16:50 +0200

    Nedvesség átlag

--- feldolg.R
+++ feldolg.R
@@ -7,3 +7,22 @@ s.dat <- as.Date(as.data.frame(s17)[,1])
 s.adat <- as.data.frame(s17)[,2:16]
 names(s.adat) <- paste0(rep(c("Forest","Forest_edge","Meadow"), each = 5), 1:5)
 s.adat[,10:15] <- numeric(nrow(s.adat))
+s.adat[s.adat == 0] <- NA
+
+## xts gyártás
+library(xts)
+moist.xts <- xts(s.adat, s.dat)
+
+## Átlag idősor
+s.atlag <- rowMeans(s.adat[,1:5], na.rm=T)
+
+## Konverziós egyenlet
+s.moist <- (0.049*s.atlag) - 98.23
+
+## Idősor
+moistmean.xts <- xts(s.moist, s.dat)
+plot(moistmean.xts)
+
+## Havi átlag
+apply.monthly(moistmean.xts, mean)
+plot(moistmean.xts)
